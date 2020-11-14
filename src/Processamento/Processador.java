1d0a9aa1750dc05a9f5836537754f089274b9dcd
Augusto Castilho <castilho@ice.ufjf.br>
2020-11-14 18:07:25 -0300

    Criacao da classe Processador

---
+++ src/Processamento/Processador.java
@@ -0,0 +1,18 @@
+package Processamento;
+
+import Instrucoes.TipoR;
+
+public class Processador {
+    public int Alu(int entrada1, int entrada2, TipoR controleAlu){
+        int resultado = 0;
+        int zero = 0;
+        switch (zero){
+                case 1:
+                    return 0;
+                default:
+                    
+                    break;
+        }
+        return resultado;
+    }
+}
