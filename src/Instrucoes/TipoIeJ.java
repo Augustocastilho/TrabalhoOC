b5c573149f7497a93424ebdb8dcd4c308964c77c
Augusto Castilho <castilho@ice.ufjf.br>
2020-11-15 19:08:32 -0300

    Iniciando metodos IeJ

--- src/Instrucoes/TipoIeJ.java
+++ src/Instrucoes/TipoIeJ.java
@@ -1,10 +1,31 @@
 package Instrucoes;
 
+/**
+ * Contem todas as operacoes de tipo I e J
+ */
 public class TipoIeJ{
     
-    int op ;
-    int rs ;
-    int rt;
-    int funct;
+    public long addi(long val, long val2){
+        return val;
+    }
     
+    public long lw(long val, long val2){
+        return val;
+    }
+    
+    public long sw(long val, long val2){
+        return val;
+    }
+    
+    public long beq(long val, long val2){
+        return val;
+    }
+    
+    public long bne(long val, long val2){
+        return val;
+    }
+    
+    public long jump(long val, long val2){
+        return val;
+    }
 }
