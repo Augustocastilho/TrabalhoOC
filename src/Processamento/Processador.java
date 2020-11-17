789180346294bd0a34f0f09465e1efe2dc5b84be
Augusto Castilho <castilho@ice.ufjf.br>
2020-11-17 04:03:33 -0300

    Sinais de controle

--- src/Processamento/Processador.java
+++ src/Processamento/Processador.java
@@ -16,7 +16,7 @@ public class Processador {
     private Instrucoes memoriaInstrucao;
     private Map<String, Long> memoriaDados;
     private Map<Integer, String> mapaInstrucoes;
-    private String nomeInstrucao; //usado para verificacao no ALU
+    private String nomeInstrucao;
     private Map<String, Long> sinaisDeControle;
     private String caminhoDoArquivo;
     private List<Map<String, Long>> registradores;
@@ -24,7 +24,7 @@ public class Processador {
 
     private long aluOut = 0;
     private long aluControl = 0;
-    
+
     private long indice;
 
     /**
@@ -37,7 +37,10 @@ public class Processador {
         this.memoriaInstrucao = pc;
         memoriaInstrucao.atribuiValores();
         this.sinaisDeControle = new HashMap<>();
-        criaSinaisControle();
+        if (memoriaInstrucao.getOp() == 0)
+            criaSinaisControleR();
+        else
+            criaSinaisControleIeJ();
         this.registradores = new LinkedList<>();
         this.memoriaDados = new HashMap<>();
         this.mapaInstrucoes = new HashMap<>();
@@ -45,71 +48,77 @@ public class Processador {
     }
 
     //MÉTODOS GETS
-    
     public String getNomeInstrucao() {
         return this.nomeInstrucao;
     }
-    
-    public long getIndice(){
+
+    public long getIndice() {
         return this.indice;
     }
-    
-    public String getCaminhoDoArquivo(){
+
+    public String getCaminhoDoArquivo() {
         return this.caminhoDoArquivo;
     }
-    
+
     public Instrucoes getMemoriaInstrucao() {
         return memoriaInstrucao;
     }
-    
+
     public Map<String, Long> getMemoriaDados() {
         return memoriaDados;
     }
-    
+
     public Map<Integer, String> getMapaInstrucoes() {
         return mapaInstrucoes;
     }
-    
+
     public Map<String, Long> getSinaisDeControle() {
         return sinaisDeControle;
     }
-    
+
     public Map<String, Long> getRegistradores() {
-        return registradores.get( (int) indice);
+        return registradores.get((int) indice);
     }
-    
+
     public Map<String, Long> getSaidasRegistrador() {
-        return saidasRegistrador.get( (int) indice);
+        return saidasRegistrador.get((int) indice);
     }
-    
+
     public long getAluOut() {
         return aluOut;
     }
-    
+
     public long getAluControl() {
         return aluControl;
     }
-    
-    
-    //Processamento    
 
-    public void criaSinaisControle() {
-        //tipos I
-        sinaisDeControle.put("RegWrite", memoriaInstrucao.getOp());
-        sinaisDeControle.put("MemToReg", memoriaInstrucao.getOp());
-        sinaisDeControle.put("Branch", memoriaInstrucao.getOp());
-        sinaisDeControle.put("MemRead", memoriaInstrucao.getOp());
-        sinaisDeControle.put("MemWrite", memoriaInstrucao.getOp());
-        sinaisDeControle.put("RegDst", memoriaInstrucao.getOp());
-        sinaisDeControle.put("ALUOp", memoriaInstrucao.getOp());
-        sinaisDeControle.put("ALUSrc", memoriaInstrucao.getOp());
-        sinaisDeControle.put("PC", memoriaInstrucao.getOp());
-        sinaisDeControle.put("ReadData1", memoriaInstrucao.getOp());
-        sinaisDeControle.put("ReadData2", memoriaInstrucao.getOp());
-        sinaisDeControle.put("ImmediateValue", memoriaInstrucao.getOp());
-        sinaisDeControle.put("rs", memoriaInstrucao.getOp());
-        sinaisDeControle.put("Destino1", memoriaInstrucao.getOp());
-        sinaisDeControle.put("Destino2", memoriaInstrucao.getOp());
+    //Processamento    
+    public void criaSinaisControleR() {
+
+        sinaisDeControle.put("ALUOp", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(30, 31)));
+        sinaisDeControle.put("ALUSrc", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(31, 31)));
+        sinaisDeControle.put("RegWrite", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(31, 31)));
+        sinaisDeControle.put("RegDst", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(31, 31)));
+        sinaisDeControle.put("MemWrite", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(31, 31)));
+        sinaisDeControle.put("MemToReg", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(31, 31)));
+        sinaisDeControle.put("MemRead", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(31, 31)));
+        sinaisDeControle.put("RegWrite", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(31, 31)));
+
+    }
+
+    public void criaSinaisControleIeJ() {
+
+        sinaisDeControle.put("ALUOp", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(0, 1)));
+        sinaisDeControle.put("ALUSrc", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(0, 0)));
+        sinaisDeControle.put("RegWrite", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(0, 0)));
+        sinaisDeControle.put("RegDst", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(0, 0)));
+        sinaisDeControle.put("MemWrite", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(0, 0)));
+        sinaisDeControle.put("MemToReg", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(0, 0)));
+        sinaisDeControle.put("MemRead", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(0, 0)));
+        sinaisDeControle.put("RegWrite", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(0, 0)));
+        sinaisDeControle.put("Jump", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(0, 0)));
+        sinaisDeControle.put("Branch", memoriaInstrucao.converteValor(memoriaInstrucao.getValor().substring(0, 0)));
+
     }
 
     /**
@@ -140,28 +149,29 @@ public class Processador {
         mapaInstrucoes.put(3, "jal");
     }
 
-    
-    private void setNomeInstrucao(){
-        switch( (int) memoriaInstrucao.getOp()){
+    private void setNomeInstrucao() {
+        switch ((int) memoriaInstrucao.getOp()) {
             case 0:
-                for(Map.Entry<Integer, String> map : mapaInstrucoes.entrySet()){
-                    if(memoriaInstrucao.getFunct() == map.getKey())
+                for (Map.Entry<Integer, String> map : mapaInstrucoes.entrySet()) {
+                    if (memoriaInstrucao.getFunct() == map.getKey()) {
                         this.nomeInstrucao = map.getValue();
+                    }
                 }
                 break;
             default:
-                for(Map.Entry<Integer, String> map : mapaInstrucoes.entrySet()){
-                    if(memoriaInstrucao.getOp() == map.getKey())
+                for (Map.Entry<Integer, String> map : mapaInstrucoes.entrySet()) {
+                    if (memoriaInstrucao.getOp() == map.getKey()) {
                         this.nomeInstrucao = map.getValue();
+                    }
                 }
                 break;
-        }          
-    } 
-    
+        }
+    }
+
     /**
-     * @param entrada1 recebe o primeiro valor vindo do registrador
+     * @param entrada1 recebe o primeiro valor vindo do registrador ou PC
      * @param entrada2 recebe o segundo valor vindo do registrador
-     * @param controleAlu indica qual operacao logica/matematica sera realizada
+     * @param controleAlu indica qual operacao logica/aritimetica sera realizada
      * @return retorna o resultado da operacao
      */
     private long alu(long entrada1, long entrada2, long controleAlu) {
@@ -172,32 +182,32 @@ public class Processador {
             case 1:
                 return 0;
             default:
-                switch (nomeInstrucao) {
-                    case "add":
+                switch ((int) controleAlu) {
+                    case 32:
                         resultado = funcoes.add(entrada1, entrada2);
                         break;
-                    case "sub":
+                    case 34:
                         resultado = funcoes.sub(entrada1, entrada2);
                         break;
-                    case "mult":
+                    case 24:
                         resultado = funcoes.mult(entrada1, entrada2);
                         break;
-                    case "div":
+                    case 26:
                         resultado = funcoes.div(entrada1, entrada2);
                         break;
-                    case "and":
+                    case 36:
                         resultado = funcoes.and(entrada1, entrada2);
                         break;
-                    case "or":
+                    case 37:
                         resultado = funcoes.or(entrada1, entrada2);
                         break;
-                    case "slt":
+                    case 42:
                         resultado = funcoes.slt(entrada1, entrada2);
                         break;
-                    case "sll":
+                    case 0:
                         resultado = funcoes.sll(entrada1, entrada2);
                         break;
-                    case "jr":
+                    case 8:
                         resultado = funcoes.jr(entrada1);
                         break;
                     default:
@@ -208,14 +218,11 @@ public class Processador {
         return resultado;
     }
 
-    private Long memoriaDeDados(Long address, Long writeData) {
+    private Long memoriaDeDados(long address, long writeData, long memWrite, long memRead) {
         memoriaDados.put("address", address);
         memoriaDados.put("writeData", writeData);
-        memoriaDados.put("MemWrite", sinaisDeControle.get("MemWrite"));
-        memoriaDados.put("MemRead", sinaisDeControle.get("MemRead"));
-
-        long memWrite = sinaisDeControle.get("MemWrite");
-        long memRead = sinaisDeControle.get("MemRead");
+        memoriaDados.put("MemWrite", memWrite);
+        memoriaDados.put("MemRead", memRead);
 
         switch ((int) memWrite) {
             case 1:
@@ -253,7 +260,7 @@ public class Processador {
             long writeData
     ) {
         registradores.add(new HashMap<>());
-        
+
         long regWrite = sinaisDeControle.get("RegWrite");
 
         registradores.get(numRegistrador).put("Read register 1", readRegister1);
@@ -269,7 +276,7 @@ public class Processador {
                 registradores.get(numRegistrador).put("Write Data", (long) 0);
                 break;
         }
-        
+
         saidasRegistrador.add(new HashMap<>());
         saidasRegistrador.get(numRegistrador).put("Read data 1", readRegister1);
         saidasRegistrador.add(new HashMap<>());
@@ -295,10 +302,10 @@ public class Processador {
                 return memoriaInstrucao.getAddress();
         }
     }
-    
+
     public int iniciaProcessador(int pc) {
         this.indice = pc;
-        memoriaDeDados(memoriaInstrucao.getValorDecimal(), (long) -1);
+        memoriaDeDados(memoriaInstrucao.getValorDecimal(), (long) -1, sinaisDeControle.get("MemWrite"), sinaisDeControle.get("MemRead"));
         if (memoriaInstrucao.getOp() == 0) {
             criaMapR();
             setNomeInstrucao();
@@ -331,7 +338,6 @@ public class Processador {
                     break;
                 default:
                     aluOut = alu(saidasRegistrador.get(pc).get("Read data 1"), saidasRegistrador.get(pc).get("Read data 2"), memoriaInstrucao.getFunct());
-
                     break;
             }
         } else {
